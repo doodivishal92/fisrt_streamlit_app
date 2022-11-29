8925458315cdd660152b65cb9c592a00c7cedcf7
Vishal <119411037+doodivishal92@users.noreply.github.com>
2022-11-29 16:13:21 +0530

    Update streamlit_app.py
    Menu and the items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My prents New Healthy Diner at Neywork')
+
+streamlit.Header('Breakfast Menu')
+
+streamlit.Text('Omega 3 and Blueberry Oat meal')
+streamlit.Text('Kale, Spinash & Rookie Smoothie')
+streamlit.Text('Hard Boiled Free-range Egg')
+
+
