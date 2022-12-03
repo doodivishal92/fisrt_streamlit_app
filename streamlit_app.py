e131507cc54aa754c860dc34304a98c3f3430f75
Vishal <119411037+doodivishal92@users.noreply.github.com>
2022-12-03 12:20:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 
 streamlit.dataframe(my_fruit_list)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
